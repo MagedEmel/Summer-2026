748736f8e85fea0648d0f1352d76f042c57c0f3b
MagedEmel <magedemel007@gmail.com>
2026-06-28 01:15:39 +0300

    s

--- js/admin-login.js
+++ js/admin-login.js
@@ -1,63 +1,57 @@
+import {
+  db,
+  collection,
+  addDoc,
+  getDocs,
+  getDoc,
+  deleteDoc,
+  updateDoc,
+  doc,
+  query,
+  where,
+  onSnapshot,
+} from "./firebase.js";
+
 // =====================================
 // Admin Login
 // =====================================
-
+let ADMIN_PASSWORD;
 // يمكنك تغيير كلمة المرور هنا
-const ADMIN_PASSWORD = "admin123";
+async function getAdminPassword() {
+  const docRef = doc(db, "admin", "pass");
+  const docSnap = await getDoc(docRef);
 
+  if (docSnap.exists()) {
+    ADMIN_PASSWORD = docSnap.data().password; // 👈 ده الـ password
+    return docSnap.data().password;
+  } else {
+    console.log("No such document!");
+  }
+}
 // =====================================
 
-function loginAdmin() {
+async function loginAdmin() {
+    const password = document.getElementById("adminPassword").value.trim();
 
-    const password = document
-        .getElementById("adminPassword")
-        .value
-        .trim();
+    const adminPassword = await getAdminPassword();
 
-    // التحقق من كلمة المرور
-
-    if (password === "") {
-
-        showToast("من فضلك أدخل كلمة المرور", "#d94343");
-
-        return;
-
-    }
-
-    if (password !== ADMIN_PASSWORD) {
-
+    if (password !== adminPassword) {
         showToast("كلمة المرور غير صحيحة", "#d94343");
-
         return;
-
     }
-
-    // حفظ حالة تسجيل الدخول
 
     localStorage.setItem("adminLogged", "true");
-
-    showToast("تم تسجيل الدخول ✅");
-
-    setTimeout(() => {
-
-        window.location.href = "admin.html";
-
-    }, 800);
-
+    window.location.href = "admin.html";
 }
-
+window.loginAdmin = loginAdmin;
 // =====================================
 // Enter Key
 // =====================================
 
 document
-    .getElementById("adminPassword")
-    .addEventListener("keypress", function (e) {
-
-        if (e.key === "Enter") {
-
-            loginAdmin();
-
-        }
-
-    });
+  .getElementById("adminPassword")
+  .addEventListener("keypress", function (e) {
+    if (e.key === "Enter") {
+      loginAdmin();
+    }
+  });
